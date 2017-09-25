affeac66106e397ed9c620162d7e13b2d8fcbcf6
Rian <riancvb@gmail.com>
2017-09-25 18:14:49 -0300

    Abas connection, db e server

--- DINo/src/main/java/br/ufsc/lisa/DINo/views/MaindApp.java
+++ DINo/src/main/java/br/ufsc/lisa/DINo/views/MaindApp.java
@@ -14,11 +14,17 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
 
 public class MaindApp {
 
 	private JFrame frame;
-	private final JComboBox comboBox = new JComboBox();
+	private JTextField textFieldHost;
+	private JTextField textFieldPort;
+	private JTextField textFieldUsername;
+	private JTextField textFieldPassword;
 
 	/**
 	 * Launch the application.
@@ -62,21 +68,67 @@ public class MaindApp {
 		});
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		comboBox.setBounds(161, 210, 158, 24);
-		comboBox.setEditable(true);
-		panel.add(comboBox);
 		
-		JLabel lblTabelas = new JLabel("Tabelas");
-		lblTabelas.setBounds(85, 214, 70, 15);
-		panel.add(lblTabelas);
+		JTabbedPane Abas = new JTabbedPane(JTabbedPane.TOP);
+		Abas.setBounds(12, 24, 283, 319);
+		panel.add(Abas);
 		
-		JButton btnButaum = new JButton("butaum");
-		btnButaum.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnButaum.setBounds(176, 92, 117, 25);
-		panel.add(btnButaum);
+		JPanel Server = new JPanel();
+		Abas.addTab("Server", null, Server, null);
+		Server.setLayout(null);
+		
+		JLabel lblHost = new JLabel("Host: ");
+		lblHost.setBounds(12, 75, 70, 15);
+		Server.add(lblHost);
+		
+		JButton btnTest = new JButton("Test");
+		btnTest.setBounds(105, 255, 65, 25);
+		Server.add(btnTest);
+		
+		textFieldHost = new JTextField();
+		textFieldHost.setBounds(61, 73, 174, 19);
+		Server.add(textFieldHost);
+		textFieldHost.setColumns(10);
+		
+		JLabel lblConnection = new JLabel("Connection");
+		lblConnection.setBounds(12, 12, 105, 25);
+		Server.add(lblConnection);
+		
+		JLabel lblPort = new JLabel("Port: ");
+		lblPort.setBounds(12, 120, 39, 15);
+		Server.add(lblPort);
+		
+		textFieldPort = new JTextField();
+		textFieldPort.setBounds(61, 118, 174, 19);
+		Server.add(textFieldPort);
+		textFieldPort.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username: ");
+		lblUsername.setBounds(12, 158, 92, 15);
+		Server.add(lblUsername);
+		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setBounds(95, 156, 140, 19);
+		Server.add(textFieldUsername);
+		textFieldUsername.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(12, 197, 92, 15);
+		Server.add(lblPassword);
+		
+		textFieldPassword = new JTextField();
+		textFieldPassword.setBounds(95, 195, 140, 19);
+		Server.add(textFieldPassword);
+		textFieldPassword.setColumns(10);
+		
+		JPanel Databases = new JPanel();
+		Abas.addTab("Databases", null, Databases, null);
+		
+		JPanel Tables = new JPanel();
+		Abas.addTab("Tables", null, Tables, null);
+		
+		JLabel lblSource = new JLabel("Source");
+		lblSource.setBounds(12, 0, 70, 15);
+		panel.add(lblSource);
 	}
 }
