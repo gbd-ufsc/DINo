22cc28c8fb06b4827e448d049ff053587e648461
Geomar Schreiner <geomarschreiner@gmail.com>
2018-04-09 09:09:26 -0300

    TimeFormatter now correct time format

--- DINo/src/main/java/br/ufsc/lisa/DINo/util/TimeFormatter.java
+++ DINo/src/main/java/br/ufsc/lisa/DINo/util/TimeFormatter.java
@@ -6,9 +6,9 @@ public abstract class TimeFormatter {
 	
 	public static String formatTime(long time) {
 		long hour = TimeUnit.MILLISECONDS.toHours(time);
-		time-=TimeUnit.MILLISECONDS.toHours(time);
+		time-=TimeUnit.HOURS.toMillis(hour);
 		long minute = TimeUnit.MILLISECONDS.toMinutes(time);
-		time-=TimeUnit.MILLISECONDS.toMinutes(time);
+		time-=TimeUnit.MINUTES.toMillis(minute);
 		long second = TimeUnit.MILLISECONDS.toSeconds(time);
 		time -= TimeUnit.SECONDS.toMillis(second);
 		
